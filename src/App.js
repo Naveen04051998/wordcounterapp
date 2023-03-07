1e15cc7c45b63e04c023a99889efe2670ea50659
nandha-kumar-hajari <nandhunaren808@gmail.com>
2023-03-07 21:05:45 +0530

    originmessage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state={value:0}
+  onChangeSearchInput = event => {
+    this.setState({
+      value: event.target.value.length
+    })
+  }
+  render() {
+    const {value}=this.state
+    return (
+      <div className='container'>
+        <div className='items-container'>
+        <h1 className="heading">Calculate the Letters you enter</h1>
+        <p className='para'>Enter the phrase</p>
+        <input type="search" className='input-element'  onChange={this.onChangeSearchInput} placeholder="Enter keyword"/>
+        <div className='container-2'> 
+        <p className='para-2'>No.of letters:<span className='para-2'>
+          {value}
+        </span>
+          </p>
+          </div>
+         
+        </div>
+        <img src="https://assets.ccbp.in/frontend/react-js/stop-watch-with-calculator-img.png" alt="letters"/>
+      </div>
+    )
+  }
 }
 
-export default App;
